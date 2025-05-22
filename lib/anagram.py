5ea45d5cb004c84524ad61d509b85411e52033bb
+ <masaitaboi@gmail.com>
2025-05-23 02:32:55 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  
+    
+    def match(self, possible_anagrams):
+        matches = []
+        
+        original_sorted = sorted(self.word)
+        
+        for candidate in possible_anagrams:
+           
+            if candidate.lower() == self.word:
+                continue
+            
+            if sorted(candidate.lower()) == original_sorted:
+                matches.append(candidate)
+        
+        return matches
